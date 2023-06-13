1507a68c4fb13c26a7a86f3331eeba0b0e8722a6
Shiroyam <shirooyam@gmail.com>
2023-06-13 18:07:05 +0300

    feat(web/app): add store

--- apps/web/src/app/providers/index.ts
+++ apps/web/src/app/providers/index.ts
@@ -1,4 +1,5 @@
 import compose from "compose-function"
 import { withRouter } from "./with-router"
+import { withRedux } from "./with-redux"
 
-export const withProviders = compose(withRouter)
+export const withProviders = compose(withRouter, withRedux)
