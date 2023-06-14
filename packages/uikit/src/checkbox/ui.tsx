f67381bbd0c57851b6f61a40b235efb1e31fd22d
Shiroyam <shirooyam@gmail.com>
2023-06-14 20:55:42 +0300

    feat(uikit): add label for checkbox

--- packages/uikit/src/checkbox/ui.tsx
+++ packages/uikit/src/checkbox/ui.tsx
@@ -2,10 +2,17 @@ import { DetailedHTMLProps, InputHTMLAttributes } from "react"
 import cn from "classnames"
 import styles from "./index.module.scss"
 
-export type CheckboxProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+export interface CheckboxProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  label?: string
+}
 
-export const Checkbox = ({ className, ...props }: CheckboxProps) => {
+export const Checkbox = ({ className, label, ...props }: CheckboxProps) => {
   const checkboxClass = cn(styles.checkbox, className)
 
-  return <input className={checkboxClass} type="checkbox" {...props} />
+  return (
+    <div className={styles.wrapper}>
+      <input className={checkboxClass} type="checkbox" {...props} />
+      {label ? <label className={styles.label}>{label}</label> : null}
+    </div>
+  )
 }
